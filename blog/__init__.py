039b1d8b09b3d88e81659dcfdba60e97881bdeb0
Raghu Reddy <raghureddy.ram@gmail.com>
2016-04-04 16:39:29 +0000

    Seed database test

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,9 +1,9 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
-from . import views
-from . import filters
+
 config_path = os.environ.get("CONFIG_PATH", "blog.config.DevelopmentConfig")
 app.config.from_object(config_path)
 
