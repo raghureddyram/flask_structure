3b53a45087bfd3f87cfc2af6e8f249525ce8836b
Raghu Reddy <raghureddy.ram@gmail.com>
2016-04-05 07:25:29 +0000

    Add templates for adding entry

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,46 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 
-from blog import app
+from . import app
 from .database import session, Entry
 
-@app.route("/")
-def entries():
-    entries = session.query(Entry)
-    entries = entries.order_by(Entry.datetime.desc())
-    entries = entries.all()
+PAGINATE_BY = 10
+
+@app.route('/')
+@app.route('/page/<int:page>')
+def entries(page=1):
+
+    page_index = page - 1
+
+    count = session.query(Entry).count()
+
+    start = page_index * PAGINATE_BY
+    end  = start + PAGINATE_BY
+
+    total_pages = (count - 1) / PAGINATE_BY + 1
+    has_next = page_index < total_pages - 1
+    has_prev = page_index > 0
+
+    entries = session.query(Entry).order_by(Entry.datetime.desc())
+    entries = entries[start:end]
+
     return render_template("entries.html",
-        entries=entries
+        entries=entries,
+        has_next=has_next,
+        has_prev=has_prev,
+        page=page,
+        total_pages=total_pages
     )
+
+@app.route("/entry/add", methods=["GET"]) #just get the template
+def add_entry_get():
+    return render_template("add_entry.html")
+
+@app.route("/entry/add", methods=["POST"])
+def add_entry_post():
+    entry = Entry(
+        title=request.form["title"],
+        content=request.form["content"],
+    )
+    session.add(entry)
+    session.commit()
+    return redirect(url_for("entries"))
